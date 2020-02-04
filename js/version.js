203ad145726b659922e01c4cc2554b03850a7e9c
Brian Brenner <bbrenner@frameshift.io>
2020-02-04 13:15:47 -0500

    updating package json

--- js/version.js
+++ js/version.js
@@ -1,10 +1,13 @@
-const _version = "2.3.6 (2ec01fc59e0d2ce08507ecfc58ecf7be59957766)"
+const _version = "2.3.6 (69db35d077f6fc54615fa9cc501e9e74c7e73cc7)"
 
 function version() {
     return _version;
 }
 
 export default version
+
+
+
 
 
 
